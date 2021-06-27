c575f769f89d4982c0f7330893153ab94b00b56d
Menard Maranan <66345147+menard-codes@users.noreply.github.com>
2021-06-27 19:09:55 +0800

    added auth context

--- auth/index.js
+++ auth/index.js
@@ -6,23 +6,17 @@ const auth = admin.auth();
 
 const retrieveUser = async authToken => {
     try {
-        const decodedToken = await auth.verifyIdToken(authToken);
+        const decodedToken = await auth.verifyIdToken(authToken)
         return decodedToken
     } catch (error) {
-        return 'Invalid Token'
+        throw new AuthenticationError(`Error 404: ${error.message}`)
     }
 }
 
 const authContext = async req => {
     const authToken = req.headers.authorization;
     const requestingUser = await retrieveUser(authToken);
-
-    if (requestingUser === 'Invalid Token') {
-        throw new AuthenticationError('Error 401: Unauthorized Access')
-    } else {
-        // user is authorized
-        return {user: requestingUser}
-    }
+    return {user: requestingUser}
 }
 
 module.exports = authContext;
--- data/index.js
+++ data/index.js
@@ -1,7 +1,6 @@
 const admin = require('../firebaseApp.js');
 
 const firestore = admin.firestore()
-
 module.exports = {
     notesCollection: firestore.collection('notes')
 }
