77c0dc1d046ee87c8382cccefd2ec4ae1c95458d
Menard Maranan <66345147+menard-codes@users.noreply.github.com>
2021-06-28 08:24:29 +0800

    nothing

--- data/index.js
+++ data/index.js
@@ -1,6 +1,6 @@
 const admin = require('../firebaseApp.js');
 
+
 const firestore = admin.firestore()
-module.exports = {
-    notesCollection: firestore.collection('notes')
-}
+
+module.exports = {notesCollection: firestore.collection('notes')}
